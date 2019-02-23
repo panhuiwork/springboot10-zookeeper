d8fc823eb810f8c1bcb32be092431ebcc07da3af
panpanwork <717006413@qq.com>
2019-02-23 13:39:27 +0800

    s

--- src/main/java/com/geral/springboot10/ticket/service/TicketServiceImpl.java
+++ src/main/java/com/geral/springboot10/ticket/service/TicketServiceImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 import com.alibaba.dubbo.config.annotation.Service;
 
 
-@Service(version="1.0.0") //dubbo的@service将服务发布出去
+@Service //dubbo的@service将服务发布出去
 @Component
 public class TicketServiceImpl implements TicketService{
 
